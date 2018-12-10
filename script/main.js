74eadcc30a4078831e65db5eda279acc59954f11
Josip Maros <j.maros95@gmail.com>
2018-12-10 23:25:55 +0100

    reset.css added, js operator function created, calculator buttons created

---
+++ script/main.js
@@ -0,0 +1,33 @@
+function add(x,y){
+    return x+y;
+}
+
+function subtract(x,y){
+    return x-y;
+}
+
+function multiply(x,y){
+    return x*y;
+}
+
+function divide(x,y){
+    return x/y;
+}
+
+function operate(operator, x, y){
+
+    switch(operator){
+        case add: 
+            add(x,y);
+            break;
+        case sub:
+            subtract(x,y);
+            break;
+        case mul:
+            multiply(x,y);
+            break;
+        case div:
+            divide(x,y);
+            break;
+    }
+}
